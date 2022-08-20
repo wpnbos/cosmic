1f06d0a6555ab4ce2bf9dc5c38bb22f2a213e779
William <william@unetiq.com>
2022-08-20 13:54:33 +0200

    Add initial domain model with tests

---
+++ model.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from datetime import date
+from typing import Any, Optional
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    order_id: str
+    sku: str
+    quantity: int
+
+
+class Batch:
+    def __init__(self, reference: str, sku: str, quantity: int, eta: Optional[date]):
+        self.reference = reference
+        self.sku = sku
+        self.eta = eta
+        self._purchased_quantity = quantity
+        self._allocations: set[OrderLine] = set()
+
+    def allocate(self, line: OrderLine) -> None:
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine) -> None:
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.quantity
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.quantity for line in self._allocations)
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, Batch):
+            return self.reference == other.reference
+        return False
---
+++ test_model.py
@@ -0,0 +1,78 @@
+from datetime import date, timedelta
+from typing import Tuple
+import pytest
+
+from model import Batch, OrderLine
+
+today = date.today()
+tomorrow = today + timedelta(days=1)
+later = tomorrow + timedelta(days=10)
+
+
+def make_batch_and_line(
+    sku: str, batch_qty: int, line_qty: int
+) -> Tuple[Batch, OrderLine]:
+    return (
+        Batch("batch=001", sku, batch_qty, eta=date.today()),
+        OrderLine("order-ref", sku, line_qty),
+    )
+
+
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+    batch, line = make_batch_and_line("SMALL_TABLE", 20, 2)
+
+    batch.allocate(line)
+
+    assert batch.available_quantity == 18
+
+
+def test_can_allocate_if_available_greater_than_required():
+    batch, line = make_batch_and_line("SMALL_TABLE", 2, 1)
+
+    assert batch.can_allocate(line)
+
+
+def test_cannot_allocate_if_available_smaller_than_required():
+    batch, line = make_batch_and_line("SMALL_TABLE", 1, 2)
+
+    assert batch.can_allocate(line) is False
+
+
+def test_can_allocate_if_available_equal_to_required():
+    batch, line = make_batch_and_line("SMALL_TABLE", 2, 2)
+
+    assert batch.can_allocate(line)
+
+
+def test_cannot_allocate_if_skus_do_no_match():
+    batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
+    different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
+    assert batch.can_allocate(different_sku_line) is False
+
+
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_and_line("CHOPSTICK-HOLDER", 10, 1)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 10
+
+
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("CHOPSTICK-HOLDER", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+
+    assert batch.available_quantity == 18
+
+
+def test_batches_with_equal_references_are_equal():
+    batch = Batch("batch-001", "CHEESE-GRATER", 4, eta=None)
+    equal_batch = Batch("batch-001", "BLACK-BOWL", 2, eta=None)
+
+    assert batch == equal_batch
+
+
+def test_batches_with_different_references_are_not_equal():
+    batch = Batch("batch-001", "CHEESE-GRATER", 4, eta=None)
+    equal_batch = Batch("batch-002", "BLACK-BOWL", 2, eta=None)
+
+    assert batch != equal_batch
