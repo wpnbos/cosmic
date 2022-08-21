65aac86465e71fc893b61bb6598efe885ccb2fd7
William <william@unetiq.com>
2022-08-21 10:25:44 +0200

    Add pytest fixtures for API and PostgreSQL

--- conftest.py
+++ conftest.py
@@ -1,8 +1,13 @@
+from pathlib import Path
+from sqlite3 import OperationalError
+import time
 import pytest
+import requests
 from sqlalchemy import create_engine
 from sqlalchemy.orm import clear_mappers, sessionmaker
 
 from orm import metadata, start_mappers
+import config
 
 
 @pytest.fixture
@@ -17,3 +22,68 @@ def session(in_memory_db):
     start_mappers()
     yield sessionmaker(bind=in_memory_db)()
     clear_mappers()
+
+
+def wait_for_postgres_to_come_up(engine):
+    deadline = time.time() + 10
+    while time.time() < deadline:
+        try:
+            return engine.connect()
+        except OperationalError:
+            time.sleep(0.5)
+    pytest.fail("Postgres never came up")
+
+
+def wait_for_api_to_come_up():
+    deadline = time.time() + 10
+    url = config.get_api_url()
+    while time.time() < deadline:
+        try:
+            return requests.get(url)
+        except ConnectionError:
+            time.sleep(0.5)
+    pytest.fail("API never came up")
+
+
+@pytest.fixture
+def add_stock(postgres_session):
+    batches_added = set()
+    skus_added = set()
+
+    def _add_stock(lines):
+        for ref, sku, quantity, eta in lines:
+            postgres_session.execute(
+                "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
+                " VALUES (:ref, :sku, :quantity, :eta)",
+                dict(ref=ref, sku=sku, quantity=quantity, eta=eta),
+            )
+            [[batch_id]] = postgres_session.execute(
+                "SELECT id FROM batches WHERE reference=:ref AND sku=:sku",
+                dict(ref=ref, sku=sku),
+            )
+            batches_added.add(batch_id)
+            skus_added.add(sku)
+        postgres_session.commit()
+
+    yield _add_stock
+
+    for batch_id in batches_added:
+        postgres_session.execute(
+            "DELETE FROM allocations WHERE batch_i=:batch_id", dict(batch_id=batch_id)
+        )
+        postgres_session.execute(
+            "DELETE FROM batches WHERE id=:batch_id", dict(batch_id=batch_id)
+        )
+    for sku in skus_added:
+        postgres_session.execute(
+            "DELETE FROM order_lines WHERE sku=:sku",
+            dict(sku=sku),
+        )
+        postgres_session.commit()
+
+
+@pytest.fixture
+def restart_api():
+    (Path(__file__).parent / "flask_app.py").touch()
+    time.sleep(0.5)
+    wait_for_api_to_come_up()
